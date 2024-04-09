8e91cdc8c543d8d5ad583c4125de07766d2b6528
Ahmed Sabry <112628761+SABRY225@users.noreply.github.com>
2024-04-09 12:25:52 +0200

    first commit

---
+++ src/Components/Prayer.jsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
+export default function MediaCard({ name, time, image }) {
+	return (
+		<Card sx={{ width: "14vw" }} className="CARD" >
+			<CardMedia
+				sx={{ height: 140 }}
+				image={image}
+				title="green iguana"
+			/>
+			<CardContent>
+				<h2>{name}</h2>
+
+				<Typography variant="h3" color="text.secondary">
+					{time}
+				</Typography>
+			</CardContent>
+		</Card>
+	);
+}
